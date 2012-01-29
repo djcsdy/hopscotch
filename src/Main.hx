bf27b79e621b0b54606920c37b78caef071139ca
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-01-29 02:36:53 +0000

    Implement the render loop in Engine.

--- src/Main.hx
+++ src/Main.hx
@@ -2,7 +2,7 @@ import hopscotch.engine.Engine;
 
 class Main {
 	static function main():Void {
-		var engine = new Engine(flash.Lib.current);
+		var engine = new Engine(flash.Lib.current, 640, 480, 60);
 		engine.start();
 	}
 }
--- src/hopscotch/engine/Engine.hx
+++ src/hopscotch/engine/Engine.hx
@@ -1,29 +1,50 @@
 package hopscotch.engine;
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import flash.display.DisplayObjectContainer;
 import flash.events.Event;
 import flash.events.IEventDispatcher;
+import flash.geom.Matrix;
 import hopscotch.errors.ArgumentError;
 import hopscotch.Playfield;
 
 class Engine {
 	public var playfield:Playfield;
 	
-	var frameEventDispatcher:IEventDispatcher;
+	var renderTarget:DisplayObjectContainer;
+	var targetBitmap:Bitmap;
+	var targetBitmapData:BitmapData;
+	var width:Int;
+	var height:Int;
 	var timeSource:ITimeSource;
-	var previousPlayfield:Playfield;
-	
-	var running:Bool;
 	var framesPerMillisecond:Float;
+	var running:Bool;
 	var startTime:Int;
+	var previousPlayfield:Playfield;
 	var previousFrame:Int;
+	var matrix:Matrix;
 	
-	public function new(frameEventDispatcher:IEventDispatcher,
+	public function new(renderTarget:DisplayObjectContainer,
+			width:Int, height:Int, framesPerSecond:Float,
 			timeSource:ITimeSource = null) {
 		
-		if (frameEventDispatcher == null) {
-			throw new ArgumentError("frameEventDispatcher is null");
+		if (renderTarget == null) {
+			throw new ArgumentError("renderTarget is null");
 		}
 		
-		this.frameEventDispatcher = frameEventDispatcher;
+		if (width <= 0) {
+			throw new ArgumentError("width out of range");
+		}
+		
+		if (height < 0) {
+			throw new ArgumentError("height out of range");
+		}
+		
+		this.renderTarget = renderTarget;
+		this.width = width;
+		this.height = height;
+		
+		setFramesPerSecond(framesPerSecond);
 		
 		if (timeSource == null) {
 			this.timeSource = new TimeSource();
@@ -32,10 +53,14 @@ class Engine {
 		}
 		
 		running = false;
-		framesPerMillisecond = 60 / 1000;
+		matrix = new Matrix();
 	}
 	
 	public function setFramesPerSecond(framesPerSecond:Float):Void {
+		if (framesPerSecond <= 0) {
+			throw new ArgumentError("framesPerSecond out of range");
+		}
+		
 		framesPerMillisecond = framesPerSecond / 1000;
 	}
 	
@@ -44,7 +69,12 @@ class Engine {
 			running = true;
 			startTime = Math.floor(timeSource.getTime() -
 					(previousFrame - 1) / framesPerMillisecond);
-			frameEventDispatcher.addEventListener(
+			
+			targetBitmapData = new BitmapData(width, height, false, 0x000000);
+			targetBitmap = new Bitmap(targetBitmapData);
+			renderTarget.addChild(targetBitmap);
+					
+			renderTarget.addEventListener(
 					Event.ENTER_FRAME, onEnterFrame);
 		}
 	}
@@ -52,7 +82,13 @@ class Engine {
 	public function stop():Void {
 		if (running) {
 			running = false;
-			frameEventDispatcher.removeEventListener(
+			
+			renderTarget.removeChild(targetBitmap);
+			targetBitmap = null;
+			targetBitmapData.dispose();
+			targetBitmapData = null;
+			
+			renderTarget.removeEventListener(
 					Event.ENTER_FRAME, onEnterFrame);
 		}
 	}
@@ -102,6 +138,11 @@ class Engine {
 	}
 	
 	function render():Void {
-		// TODO
+		targetBitmapData.fillRect(targetBitmapData.rect, 0x000000);
+		
+		if (playfield != null) {
+			matrix.identity();
+			playfield.render(targetBitmapData, matrix);
+		}
 	}
 }
