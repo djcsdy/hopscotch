06eb80dc3b1028b35e49b4a8418fff32f3c05293
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-01-29 11:56:03 +0000

    Give IGraphic begin() and end() functions also. It could be useful for graphics to know when they go in and out of use, but I don't see any reason why things that implement both IUpdater and IGraphic should need separate methods.

--- src/hopscotch/IGraphic.hx
+++ src/hopscotch/IGraphic.hx
@@ -3,6 +3,8 @@ import flash.display.BitmapData;
 import flash.geom.Matrix;
 
 interface IGraphic {
+	function begin(frame:Int):Void;
+	function end():Void;
 	function updateGraphic(frame:Int):Void;
 	function render(target:BitmapData, camera:Matrix):Void;
 }
