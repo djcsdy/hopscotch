17971b86e9609330160bee9548fe6375154fbf57
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-03-18 21:34:08 +0000

    Fix Image and Static classes to actually work.

--- src/hopscotch/Static.hx
+++ src/hopscotch/Static.hx
@@ -1,5 +1,7 @@
 package hopscotch;
 
+import flash.geom.Point;
+
 class Static {
     public static var zero:Point = new Point();
 }
--- src/hopscotch/graphics/Image.hx
+++ src/hopscotch/graphics/Image.hx
@@ -1,18 +1,18 @@
 package hopscotch.graphics;
 
+import flash.geom.Matrix;
 import flash.geom.Rectangle;
 import flash.display.BitmapData;
 import hopscotch.errors.ArgumentError;
 
-class Image {
-    public var active:Boolean;
-    public var visible:Boolean;
+class Image implements IGraphic {
+    public var active:Bool;
+    public var visible:Bool;
 
     var source:BitmapData;
     var sourceRect:Rectangle;
 
     var buffer:BitmapData;
-    var bitmap:Bitmap;
 
     public function new (source:BitmapData, clipRect:Rectangle = null) {
         if (source == null) {
@@ -25,15 +25,24 @@ class Image {
         this.source = source;
 
         if (clipRect == null) {
-            sourceRect = source.rectangle;
+            sourceRect = source.rect;
         }
 
-        buffer = new BitmapData(sourceRect.width, sourceRect.height, true);
+        buffer = new BitmapData(cast sourceRect.width, cast sourceRect.height, true);
         buffer.copyPixels(source, sourceRect, Static.zero);
     }
 
-    override public function render(target:BitmapData, camera:Matrix):Void {
+    public function beginGraphic (frame:Int):Void {
+    }
+
+    public function endGraphic ():Void {
+    }
+
+    public function updateGraphic (frame:Int):Void {
+    }
+
+    public function render (target:BitmapData, camera:Matrix):Void {
         // TODO draw this in the correct place.
-        target.copyPixels(buffer, buffer.rectangle, Static.zero, null, null, true);
+        target.copyPixels(buffer, buffer.rect, Static.zero, null, null, true);
     }
 }
