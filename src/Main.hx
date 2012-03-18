c0e48d6df0d2331ddb64cc70c70f541db31369d2
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-03-18 19:12:16 +0000

    Uselessly add an entity to the playfield, for testing.

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,4 @@
+import hopscotch.Entity;
 import hopscotch.engine.Engine;
 import hopscotch.Playfield;
 
@@ -5,6 +6,7 @@ class Main {
     static function main ():Void {
         var engine = new Engine(flash.Lib.current, 640, 480, 60);
         engine.playfield = new Playfield();
+        engine.playfield.addEntity(new Entity());
         engine.start();
     }
 }
