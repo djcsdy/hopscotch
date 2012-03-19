76a68afd6a1bc2a34ed774bdf1cfadb8bdbdf038
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-03-19 00:43:36 +0000

    Add some more statically allocated objects for performance & convenience.

--- src/hopscotch/Static.hx
+++ src/hopscotch/Static.hx
@@ -1,7 +1,13 @@
 package hopscotch;
 
+import flash.geom.Matrix;
 import flash.geom.Point;
 
 class Static {
     public static var zero:Point = new Point();
+    public static var point:Point = new Point();
+    public static var point2:Point = new Point();
+
+    public static var identity:Matrix = new Matrix();
+    public static var matrix:Matrix = new Matrix();
 }
