ccc91af9e37ac5b28aa0cb94c4b35f9398d8ccff
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-03-25 16:41:08 +0100

    Reference the new input classes from Main so we can prove that they compile.

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,7 @@
+import flash.Lib;
+import hopscotch.input.digital.Keyboard;
+import hopscotch.input.digital.Button;
+import hopscotch.input.digital.Key;
 import hopscotch.graphics.GraphicList;
 import hopscotch.graphics.Image;
 import hopscotch.Entity;
@@ -6,13 +10,15 @@ import hopscotch.Playfield;
 
 class Main {
     static function main ():Void {
-        var engine = new Engine(flash.Lib.current, 640, 480, 60);
+        var engine = new Engine(Lib.current, 640, 480, 60);
         engine.playfield = new Playfield();
         var entity = new Entity();
         engine.playfield.addEntity(entity);
         var graphicList = new GraphicList();
         entity.setGraphic(graphicList);
         graphicList.graphics.push(new Image(null));
+        var keyboard = new Keyboard(Lib.current.stage);
+        engine.inputs.push(keyboard.buttonForKey(Key.X));
         engine.start();
     }
 }
