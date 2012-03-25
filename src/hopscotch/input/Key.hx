072cbceb500e38d340069efaa9e4d4416e75bc9f
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-03-25 13:56:38 +0100

    Add enum for keyboard keys.

---
+++ src/hopscotch/input/Key.hx
@@ -0,0 +1,110 @@
+package hopscotch.input;
+
+enum Key {
+    A;
+    B;
+    C;
+    D;
+    E;
+    F;
+    G;
+    H;
+    I;
+    J;
+    K;
+    L;
+    M;
+    N;
+    O;
+    P;
+    Q;
+    R;
+    S;
+    T;
+    U;
+    V;
+    W;
+    X;
+    Y;
+    Z;
+
+    Number0;
+    Number1;
+    Number2;
+    Number3;
+    Number4;
+    Number5;
+    Number6;
+    Number7;
+    Number8;
+    Number9;
+
+    Backtick;
+    Backslash;
+    Comma;
+    Equals;
+    LeftBracket;
+    Minus;
+    Period;
+    Quote;
+    RightBracket;
+    Semicolon;
+    Slash;
+    Space;
+    Tab;
+
+    Numpad0;
+    Numpad1;
+    Numpad2;
+    Numpad3;
+    Numpad4;
+    Numpad5;
+    Numpad6;
+    Numpad7;
+    Numpad8;
+    Numpad9;
+    NumpadAdd;
+    NumpadDecimal;
+    NumpadDivide;
+    NumpadEnter;
+    NumpadMultiply;
+    NumpadSubtract;
+
+    Up;
+    Down;
+    Left;
+    Right;
+
+    Backspace;
+    Delete;
+    End;
+    Enter;
+    Home;
+    Insert;
+    PageDown;
+    PageUp;
+
+    F1;
+    F2;
+    F3;
+    F4;
+    F5;
+    F6;
+    F7;
+    F8;
+    F9;
+    F10;
+    F11;
+    F12;
+    F13;
+    F14;
+    F15;
+
+    Alt;
+    CapsLock;
+    Command;
+    Control;
+    Shift;
+
+    Escape;
+}
