c713f982114111829fe03140a3eb06c50a51e59e
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-05-21 23:12:46 +0100

    Add base Graphic class as no-op implementation of IGraphic.

---
+++ src/hopscotch/graphics/Graphic.hx
@@ -0,0 +1,27 @@
+package hopscotch.graphics;
+
+import flash.geom.Matrix;
+import flash.geom.Point;
+import flash.display.BitmapData;
+import hopscotch.engine.ScreenSize;
+class Graphic implements IGraphic {
+    public var active:Bool;
+    public var visible:Bool;
+
+    public function new() {
+        active = false;
+        visible = true;
+    }
+
+    public function beginGraphic(frame:Int) {
+    }
+
+    public function endGraphic() {
+    }
+
+    public function updateGraphic(frame:Int, screenSize:ScreenSize) {
+    }
+
+    public function render(target:BitmapData, position:Point, camera:Matrix) {
+    }
+}
