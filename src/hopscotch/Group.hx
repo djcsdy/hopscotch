05c907c35bb16db9cee42604be2e2f294a78eef1
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-06-02 14:06:52 +0100

    Extract entity/updater/graphic registration from Playfield into a generic 'Group' that can be used to group together entities/updaters/graphics.

---
+++ src/hopscotch/Group.hx
@@ -0,0 +1,192 @@
+package hopscotch;
+import flash.geom.Matrix;
+import flash.display.BitmapData;
+import flash.geom.Point;
+import hopscotch.engine.ScreenSize;
+import hopscotch.errors.IllegalOperationError;
+import hopscotch.errors.ArgumentNullError;
+import hopscotch.graphics.IGraphic;
+
+class Group implements IEntity {
+    public var active:Bool;
+    public var visible:Bool;
+
+    var updateFrame:Int;
+    var graphicFrame:Int;
+
+    var updaters:Array<IUpdater>;
+    var graphics:Array<IGraphic>;
+
+    public function new () {
+        active = true;
+        visible = true;
+
+        updateFrame = -1;
+        graphicFrame = -1;
+
+        updaters = [];
+        graphics = [];
+    }
+
+    public function addUpdater (updater:IUpdater) {
+        if (updater == null) {
+            throw new ArgumentNullError("updater");
+        }
+
+        updaters[updaters.length] = updater;
+
+        if (updateFrame >= 0) {
+            updater.begin(updateFrame);
+        }
+    }
+
+    public function removeUpdater (updater:IUpdater) {
+        if (updater == null) {
+            throw new ArgumentNullError("updater");
+        }
+
+        var i = Lambda.indexOf(updaters, updater);
+        if (i < 0) {
+            throw new IllegalOperationError("Tried to remove an updater "
+                    + "that was never added");
+        }
+
+        updaters.splice(i, 1);
+
+        if (updateFrame >= 0) {
+            updater.end();
+        }
+    }
+
+    public function addGraphic (graphic:IGraphic) {
+        if (graphic == null) {
+            throw new ArgumentNullError("graphic");
+        }
+
+        graphics[graphics.length] = graphic;
+
+        if (graphicFrame >= 0) {
+            graphic.beginGraphic(graphicFrame);
+        }
+    }
+
+    public function removeGraphic (graphic:IGraphic) {
+        if (graphic == null) {
+            throw new ArgumentNullError("graphic");
+        }
+
+        var i = Lambda.indexOf(graphics, graphic);
+        if (i < 0) {
+            throw new IllegalOperationError("Tried to remove a graphic "
+                    + "that was never added.");
+        }
+
+        graphics.splice(i, 1);
+
+        if (graphicFrame >= 0) {
+            graphic.endGraphic();
+        }
+    }
+
+    public function addEntity (entity:IEntity) {
+        if (entity == null) {
+            throw new ArgumentNullError("entity");
+        }
+
+        updaters[updaters.length] = entity;
+        graphics[graphics.length] = entity;
+
+        if (updateFrame >= 0) {
+            entity.begin(updateFrame);
+        }
+
+        if (graphicFrame >= 0) {
+            entity.beginGraphic(graphicFrame);
+        }
+    }
+
+    public function removeEntity (entity:IEntity) {
+        if (entity == null) {
+            throw new ArgumentNullError("entity");
+        }
+
+        var updatersIndex = Lambda.indexOf(updaters, entity);
+        var graphicsIndex = Lambda.indexOf(graphics, entity);
+
+        if (updatersIndex < 0 || graphicsIndex < 0) {
+            throw new IllegalOperationError("Tried to remove an entity "
+                    + "that was never added");
+        }
+
+        updaters.splice(updatersIndex, 1);
+        graphics.splice(graphicsIndex, 1);
+
+        if (updateFrame >= 0) {
+            entity.end();
+        }
+
+        if (graphicFrame >= 0) {
+            entity.endGraphic();
+        }
+    }
+
+    public function begin (frame:Int) {
+        updateFrame = frame;
+
+        for (updater in updaters) {
+            updater.begin(frame);
+        }
+    }
+
+    public function end () {
+        for (updater in updaters) {
+            updater.end();
+        }
+
+        updateFrame = -1;
+    }
+
+    public function update (frame:Int) {
+        updateFrame = frame;
+
+        for (updater in updaters) {
+            if (updater.active) {
+                updater.update(frame);
+            }
+        }
+    }
+
+    public function beginGraphic (frame:Int) {
+        graphicFrame = frame;
+
+        for (graphic in graphics) {
+            graphic.beginGraphic(frame);
+        }
+    }
+
+    public function endGraphic () {
+        for (graphic in graphics) {
+            graphic.endGraphic();
+        }
+
+        graphicFrame = -1;
+    }
+
+    public function updateGraphic (frame:Int, screenSize:ScreenSize) {
+        graphicFrame = frame;
+
+        for (graphic in graphics) {
+            if (graphic.active) {
+                graphic.updateGraphic(frame, screenSize);
+            }
+        }
+    }
+
+    public function render (target:BitmapData, position:Point, camera:Matrix) {
+        for (graphic in graphics) {
+            if (graphic.visible) {
+                graphic.render(target, position, camera);
+            }
+        }
+    }
+}
--- src/hopscotch/Playfield.hx
+++ src/hopscotch/Playfield.hx
@@ -15,11 +15,7 @@ class Playfield {
 
     public var camera:ICamera;
 
-    var updateFrame:Int;
-    var graphicFrame:Int;
-
-    var updaters:Array<IUpdater>;
-    var graphics:Array<IGraphic>;
+    var group:Group;
 
     var previousCamera:ICamera;
     var cameraMatrix:Matrix;
@@ -29,150 +25,51 @@ class Playfield {
         active = true;
         visible = true;
 
-        updateFrame = -1;
-        graphicFrame = -1;
+        group = new Group();
 
-        updaters = new Array<IUpdater>();
-        graphics = new Array<IGraphic>();
-
+        previousCamera = null;
         cameraMatrix = new Matrix();
         tmpMatrix = new Matrix();
     }
 
-    public function addUpdater (updater:IUpdater):Void {
-        if (updater == null) {
-            throw new ArgumentNullError("updater");
-        }
-
-        updaters[updaters.length] = updater;
-
-        if (updateFrame >= 0) {
-            updater.begin(updateFrame);
-        }
+    public function addUpdater (updater:IUpdater) {
+        group.addUpdater(updater);
     }
 
-    public function removeUpdater (updater:IUpdater):Void {
-        if (updater == null) {
-            throw new ArgumentNullError("updater");
-        }
-
-        var i = Lambda.indexOf(updaters, updater);
-        if (i < 0) {
-            throw new IllegalOperationError("Tried to remove an updater "
-                    + "that was never added");
-        }
-
-        updaters.splice(i, 1);
-
-        if (updateFrame >= 0) {
-            updater.end();
-        }
+    public function removeUpdater (updater:IUpdater) {
+        group.removeUpdater(updater);
     }
 
-    public function addGraphic (graphic:IGraphic):Void {
-        if (graphic == null) {
-            throw new ArgumentNullError("graphic");
-        }
-
-        graphics[graphics.length] = graphic;
-
-        if (graphicFrame >= 0) {
-            graphic.beginGraphic(graphicFrame);
-        }
+    public function addGraphic (graphic:IGraphic) {
+        group.addGraphic(graphic);
     }
 
-    public function removeGraphic (graphic:IGraphic):Void {
-        if (graphic == null) {
-            throw new ArgumentNullError("graphic");
-        }
-
-        var i = Lambda.indexOf(graphics, graphic);
-        if (i < 0) {
-            throw new IllegalOperationError("Tried to remove a graphic "
-                    + "that was never added");
-        }
-
-        graphics.splice(i, 1);
-
-        if (graphicFrame >= 0) {
-            graphic.endGraphic();
-        }
+    public function removeGraphic (graphic:IGraphic) {
+        group.removeGraphic(graphic);
     }
 
-    public function addEntity (entity:IEntity):Void {
-        if (entity == null) {
-            throw new ArgumentNullError("entity");
-        }
-
-        updaters[updaters.length] = entity;
-        graphics[graphics.length] = entity;
-
-        if (updateFrame >= 0) {
-            entity.begin(updateFrame);
-        }
-
-        if (graphicFrame >= 0) {
-            entity.beginGraphic(graphicFrame);
-        }
+    public function addEntity (entity:IEntity) {
+        group.addEntity(entity);
     }
 
-    public function removeEntity (entity:IEntity):Void {
-        if (entity == null) {
-            throw new ArgumentNullError("entity");
-        }
-
-        var updatersIndex = Lambda.indexOf(updaters, entity);
-        var graphicsIndex = Lambda.indexOf(graphics, entity);
-
-        if (updatersIndex < 0 || graphicsIndex < 0) {
-            throw new IllegalOperationError("Tried to remove an entity "
-                    + "that was never added");
-        }
-
-        updaters.splice(updatersIndex, 1);
-        graphics.splice(graphicsIndex, 1);
-
-        if (updateFrame >= 0) {
-            entity.end();
-        }
-
-        if (graphicFrame >= 0) {
-            entity.endGraphic();
-        }
+    public function removeEntity (entity:IEntity) {
+        group.removeEntity(entity);
     }
 
-    public function begin (frame:Int):Void {
-        updateFrame = frame;
-
-        for (updater in updaters) {
-            updater.begin(frame);
-        }
+    public function begin (frame:Int) {
+        group.begin(frame);
     }
 
     public function end ():Void {
-        for (updater in updaters) {
-            updater.end();
-        }
-
-        updateFrame = -1;
+        group.end();
     }
 
-    public function update (frame:Int):Void {
-        updateFrame = frame;
-
-        for (updater in updaters) {
-            if (updater.active) {
-                updater.update(frame);
-            }
-        }
+    public function update (frame:Int) {
+        group.update(frame);
     }
 
-    public function beginGraphic (frame:Int):Void {
-        graphicFrame = frame;
-
-        for (graphic in graphics) {
-            graphic.beginGraphic(frame);
-        }
+    public function beginGraphic (frame:Int) {
+        group.beginGraphic(frame);
 
         if (camera != null) {
             camera.begin(frame);
@@ -180,27 +77,17 @@ class Playfield {
         previousCamera = camera;
     }
 
-    public function endGraphic ():Void {
-        for (graphic in graphics) {
-            graphic.endGraphic();
-        }
+    public function endGraphic () {
+        group.endGraphic();
 
         if (camera != null) {
             camera.end();
         }
         previousCamera = null;
-
-        graphicFrame = -1;
     }
 
-    public function updateGraphic (frame:Int, screenSize:ScreenSize):Void {
-        graphicFrame = frame;
-
-        for (graphic in graphics) {
-            if (graphic.active) {
-                graphic.updateGraphic(frame, screenSize);
-            }
-        }
+    public function updateGraphic (frame:Int, screenSize:ScreenSize) {
+        group.updateGraphic(frame, screenSize);
 
         if (camera != previousCamera) {
             if (previousCamera != null) {
@@ -230,10 +117,6 @@ class Playfield {
         tmpMatrix.ty = cameraMatrix.ty + position.y;
         tmpMatrix.concat(camera);
 
-        for (graphic in graphics) {
-            if (graphic.visible) {
-                graphic.render(target, Static.origin, tmpMatrix);
-            }
-        }
+        group.render(target, Static.origin, tmpMatrix);
     }
 }
