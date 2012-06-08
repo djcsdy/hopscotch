01a0ac00198dcbe6c66f251e8b4ba2f10c79ed30
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-06-08 15:15:32 +0100

    NME build script and trivial test harness.

---
+++ test/hopscotch/TestMain.hx
@@ -0,0 +1,10 @@
+package hopscotch;
+
+import haxe.unit.TestRunner;
+
+class TestMain {
+    static function main() {
+        var testRunner = new TestRunner();
+        testRunner.run();
+    }
+}
