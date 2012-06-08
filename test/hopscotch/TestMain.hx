358f141c127f3b0b8a52cabff9bdc9fe397c59a5
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-06-08 15:42:57 +0100

    Some *really* noddy test cases to prove the Engine constructor handles arguments as expected.

--- test/hopscotch/TestMain.hx
+++ test/hopscotch/TestMain.hx
@@ -1,10 +1,12 @@
 package hopscotch;
 
+import hopscotch.engine.EngineTest;
 import haxe.unit.TestRunner;
 
 class TestMain {
     static function main() {
         var testRunner = new TestRunner();
+        testRunner.add(new EngineTest());
         testRunner.run();
     }
 }
---
+++ test/hopscotch/engine/EngineTest.hx
@@ -0,0 +1,54 @@
+package hopscotch.engine;
+
+import hopscotch.errors.ArgumentError;
+import flash.Lib;
+import hopscotch.errors.ArgumentNullError;
+import haxe.unit.TestCase;
+
+class EngineTest extends TestCase {
+    public function new() {
+        super();
+    }
+
+    public function testNullRenderTarget() {
+        var caught = false;
+        try {
+            new Engine(null, 640, 480, 60);
+        } catch (e:ArgumentNullError) {
+            caught = true;
+        }
+        assertTrue(caught);
+    }
+
+    public function testInvalidSize() {
+        for (s in [0, -1, -20]) {
+            var caught = false;
+            try {
+                new Engine(Lib.current, s, 480, 60);
+            } catch (e:ArgumentError) {
+                caught = true;
+            }
+            assertTrue(caught);
+
+            caught = false;
+            try {
+                new Engine(Lib.current, 640, s, 60);
+            } catch (e:ArgumentError) {
+                caught = true;
+            }
+            assertTrue(caught);
+        }
+    }
+
+    public function testInvalidFramesPerSecond() {
+        for (fps in [0, -1, -20]) {
+            var caught = false;
+            try {
+                new Engine(Lib.current, 640, 480, fps);
+            } catch (e:ArgumentError) {
+                caught = true;
+            }
+            assertTrue(caught);
+        }
+    }
+}
