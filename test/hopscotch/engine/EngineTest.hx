0edcb7deaeef22c47a5f5776f1a251c1e3076100
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-06-11 10:57:49 +0100

    Normalize formatting.

--- test/hopscotch/engine/EngineTest.hx
+++ test/hopscotch/engine/EngineTest.hx
@@ -8,11 +8,11 @@ import hopscotch.errors.ArgumentNullError;
 import haxe.unit.TestCase;
 
 class EngineTest extends TestCase {
-    public function new() {
+    public function new () {
         super();
     }
 
-    public function testNullRenderTarget() {
+    public function testNullRenderTarget () {
         var caught = false;
         try {
             new Engine(null, 640, 480, 60);
@@ -22,7 +22,7 @@ class EngineTest extends TestCase {
         assertTrue(caught);
     }
 
-    public function testInvalidSize() {
+    public function testInvalidSize () {
         for (s in [0, -1, -20]) {
             var caught = false;
             try {
@@ -42,7 +42,7 @@ class EngineTest extends TestCase {
         }
     }
 
-    public function testInvalidFramesPerSecond() {
+    public function testInvalidFramesPerSecond () {
         for (fps in [0, -1, -20]) {
             var caught = false;
             try {
@@ -54,7 +54,7 @@ class EngineTest extends TestCase {
         }
     }
 
-    public function testRunningIsSetAsExpected() {
+    public function testRunningIsSetAsExpected () {
         var engine = new Engine(Lib.current, 640, 480, 60);
         assertFalse(engine.running);
 
@@ -71,7 +71,7 @@ class EngineTest extends TestCase {
         assertFalse(engine.running);
     }
 
-    public function testNullPlayfieldIsTolerated() {
+    public function testNullPlayfieldIsTolerated () {
         var renderTarget = new Sprite();
         var timeSource = new TimeSourceMock();
         var engine = new Engine(renderTarget, 640, 480, 60, timeSource);
@@ -88,9 +88,9 @@ class EngineTest extends TestCase {
         assertTrue(true);
     }
 
-    public function testUpdateIsCalledAsExpected() {
+    public function testUpdateIsCalledAsExpected () {
         var fps = 60;
-        var frameInterval = 1000/60;
+        var frameInterval = 1000 / 60;
 
         var renderTarget = new Sprite();
         var timeSource = new TimeSourceMock();
@@ -103,11 +103,11 @@ class EngineTest extends TestCase {
         engine.start();
 
         for (i in 0...10) {
-            timeSource.time = i*i;
+            timeSource.time = i * i;
             renderTarget.dispatchEvent(new Event(Event.ENTER_FRAME));
 
             var frame = Math.floor(timeSource.time / frameInterval);
-            assertEquals(frame+1, playfield.updateCount);
+            assertEquals(frame + 1, playfield.updateCount);
             assertEquals(frame, playfield.updateFrame);
         }
     }
