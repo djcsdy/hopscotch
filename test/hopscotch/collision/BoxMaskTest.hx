8cabad08ce9f13f2c06649f6b00e75145b730d04
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-06-23 01:15:08 +0100

    Add a few more simple tests for BoxMask. That should do it.

--- test/hopscotch/collision/BoxMaskTest.hx
+++ test/hopscotch/collision/BoxMaskTest.hx
@@ -17,8 +17,31 @@ class BoxMaskTest extends TestCase {
         assertTrue(boxMask.collide(boxMask));
     }
 
+    public function testBoxMaskCollidesWithItselfAtOverlappingCoordinates () {
+        var boxMask = new BoxMask(5, 5, 10, 10);
+        assertTrue(boxMask.collide(boxMask, -4, 0, 4, 9));
+    }
+
     public function testBoxDoesNotCollideWithItselfAtDifferentCoordinates () {
         var boxMask = new BoxMask(5, 5, 10, 10);
         assertFalse(boxMask.collide(boxMask, -5, -10, 10, 10));
+    }
+
+    public function testBoxMaskCollidesWithAnotherBoxMask () {
+        var boxMask1 = new BoxMask(5, 5, 10, 10);
+        var boxMask2 = new BoxMask(-2, 0, 3, 4);
+        assertTrue(boxMask1.collide(boxMask2, -20, 5, -4, 19));
+    }
+
+    public function testBoxMaskDoesNotCollideWithAnotherBoxMask () {
+        var boxMask1 = new BoxMask(5, 5, 10, 10);
+        var boxMask2 = new BoxMask(-2, 0, 3, 4);
+        assertFalse(boxMask1.collide(boxMask2, -20, 5, -50, 0));
+    }
+
+    public function testBoxMaskDoesNotCollideWithAnAdjacentBoxMask () {
+        var boxMask1 = new BoxMask(5, 5, 10, 10);
+        var boxMask2 = new BoxMask(-2, 0, 3, 4);
+        assertFalse(boxMask1.collide(boxMask2, -20, 5, -16, 19));
     }
 }
