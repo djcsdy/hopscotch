2a93cb8846540278c022a47899325a256436b3e5
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-07-31 20:43:20 +0100

    Make _typewriter font available via FontFace.

--- src/hopscotch/graphics/FontFace.hx
+++ src/hopscotch/graphics/FontFace.hx
@@ -3,8 +3,9 @@ package hopscotch.graphics;
 import hopscotch.errors.ArgumentNullError;
 
 class FontFace {
-    public static var sans(default, null) = new FontFace("_sans", false);
-    public static var serif(default, null) = new FontFace("_serif", false);
+    public static var sans(default, null):FontFace = new FontFace("_sans", false);
+    public static var serif(default, null):FontFace = new FontFace("_serif", false);
+    public static var typewriter(default, null):FontFace = new FontFace("_typewriter", false);
 
     public var name(default, null):String;
     public var embedded(default, null):Bool;
