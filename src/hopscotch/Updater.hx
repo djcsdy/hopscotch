6b521ef9301f87ac9bfaecea1d69224913a51fd4
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-08-16 00:19:48 +0100

    Add convenience base class implementing IUpdater.

---
+++ src/hopscotch/Updater.hx
@@ -0,0 +1,18 @@
+package hopscotch;
+
+class Updater implements IUpdater {
+    public var active:Bool;
+
+    public function new() {
+        active = true;
+    }
+
+    public function begin(frame:Int) {
+    }
+
+    public function end() {
+    }
+
+    public function update(frame:Int) {
+    }
+}
