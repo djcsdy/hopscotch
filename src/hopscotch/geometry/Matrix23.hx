e125cb4c0bb5b773861ec3920bb83f3cf37a38f3
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-10-17 17:01:20 +0100

    Add various matrix manipulation functions.

--- src/hopscotch/geometry/Matrix23.hx
+++ src/hopscotch/geometry/Matrix23.hx
@@ -9,6 +9,11 @@ package hopscotch.geometry;
  * and scaling), which can then be applied to a Vector2d or other geometry in a
  * single step.
  *
+ * The identity matrix can be thought of as an empty box that is ready to store
+ * a sequence of transformations. The identity matrix is a matrix that applies
+ * the null or identity affine transformation; an object that is transformed by
+ * the identity matrix will be identical to the original.
+ *
  * The matrix values are arranged in the following orientation.
  *
  * <table><tr><td>a</td><td>c</td><td>tx</td></tr>
@@ -59,5 +64,160 @@ class Matrix23 {
         this.d = d;
         this.tx = tx;
         this.ty = ty;
+    }
+
+    // This function is defined as a more convenient way to call the equivalent
+    // static function. It is declared inline so that it will not create an
+    // extra property on every instance of Matrix23. This reduces memory
+    // consumption on some platforms.
+    //
+    /** Sets the properties of the matrix so that it is the identity matrix.
+     *
+     * The identity matrix can be thought of as an empty box that is ready to
+     * store a sequence of transformations. The identity matrix is a matrix
+     * that applies the null or identity affine transformation; an object that
+     * is transformed by the identity matrix will be identical to the original.
+     *
+     * An identity matrix has the values a=1, b=0, c=0, d=1, tx=0, ty=0. */
+    public inline function identity() {
+        Matrix23.identity(this);
+    }
+
+    // This function is defined as a more convenient way to call the equivalent
+    // static function. It is declared inline so that it will not create an
+    // extra property on every instance of Matrix23. This reduces memory
+    // consumption on some platforms.
+    //
+    /** Applies a scaling transformation to the matrix. The x-axis is scaled
+     * by sx, and the y-axis is scaled by sy. */
+    public inline function scale(sx:Float, sy:Float) {
+        Matrix23.scale(this, sx, sy);
+    }
+
+    // This function is defined as a more convenient way to call the equivalent
+    // static function. It is declared inline so that it will not create an
+    // extra property on every instance of Matrix23. This reduces memory
+    // consumption on some platforms.
+    //
+    /** Applies a rotation transformation to the matrix. The angle of rotation
+     * is clockwise and is measured in radians. */
+    public inline function rotate(radians:Float) {
+        Matrix23.rotate(this, radians);
+    }
+
+    // This function is defined as a more convenient way to call the equivalent
+    // static function. It is declared inline so that it will not create an
+    // extra property on every instance of Matrix23. This reduces memory
+    // consumption on some platforms.
+    //
+    /** Applies a translation transformation to the matrix. The translation is
+     * tx units along the x-axis, and ty pixels along the y-axis. */
+    public inline function translate(tx:Float, ty:Float) {
+        Matrix23.translate(this, tx, ty);
+    }
+
+    // This function is defined as a more convenient way to call the equivalent
+    // static function. It is declared inline so that it will not create an
+    // extra property on every instance of Matrix23. This reduces memory
+    // consumption on some platforms.
+    //
+    /** Concatenates a matrix with the current matrix. The current matrix is
+     * replaced by the concatenated matrix.
+     *
+     * This effectively combines the affine transformations represented by the
+     * two matrices. For example, if the current matrix represents a
+     * transformation that scales an object by four times, and the second
+     * matrix represents a transformation that rotates an object by pi radians
+     * (a half-turn), then the combined matrix will represent a transformation
+     * that is equivalent to scaling an object by four times and then rotating
+     * it by pi radians.
+     *
+     * In mathematical terms this operation is equivalent to matrix
+     * multiplication. */
+    public inline function concat(matrix:Matrix23) {
+        Matrix23.concat(this, matrix);
+    }
+
+    /** Sets the properties of the specified matrix so that it is the identity
+     * matrix.
+     *
+     * The identity matrix can be thought of as an empty box that is ready to
+     * store a sequence of transformations. The identity matrix is a matrix
+     * that applies the null or identity affine transformation; an object that
+     * is transformed by the identity matrix will be identical to the original.
+     *
+     * An identity matrix has the values a=1, b=0, c=0, d=1, tx=0, ty=0. */
+    public static function identity(matrix:Matrix23) {
+        matrix.a = matrix.d = 1;
+        matrix.b = matrix.c = matrix.tx = matrix.ty = 0;
+    }
+
+    /** Applies a scaling transformation to the specified matrix. The x-axis is
+     * scaled by sx, and the y-axis is scaled by sy. */
+    public static function scale(matrix:Matrix23, sx:Float, sy:Float) {
+        matrix.a *= sx;
+        matrix.b *= sy;
+        matrix.c *= sx;
+        matrix.d *= sy;
+        matrix.tx *= sx;
+        matrix.ty *= sy;
+    }
+
+    /** Applies a rotation transformation to the specified matrix. The angle of
+     * rotation is clockwise and is measured in radians. */
+    public static function rotate(matrix:Matrix23, radians:Float) {
+        var cos = Math.cos(radians);
+        var sin = Math.sin(radians);
+
+        var a = matrix.a;
+        var b = matrix.b;
+        var c = matrix.c;
+        var d = matrix.d;
+        var tx = matrix.tx;
+        var ty = matrix.ty;
+
+        matrix.a = cos * a + -sin * b;
+        matrix.b = sin * a + cos * b;
+        matrix.c = cos * c + -sin * d;
+        matrix.d = sin * c + cos * d;
+        matrix.tx = cos * tx + -sin * ty;
+        matrix.ty = sin * tx + cos * ty;
+    }
+
+    /** Applies a translation transformation to the specified matrix. The
+     * translation is tx units along the x-axis, and ty pixels along the
+     * y-axis. */
+    public static function translate(matrix:Matrix23, tx:Float, ty:Float) {
+        matrix.tx += tx;
+        matrix.ty += ty;
+    }
+
+    /** Concatenates two matrices. The first matrix is replaced by the
+     * concatenated matrix.
+     *
+     * This effectively combines the affine transformations represented by the
+     * two matrices. For example, if the first matrix represents a
+     * transformation that scales an object by four times, and the second
+     * matrix represents a transformation that rotates an object by pi radians
+     * (a half-turn), then the combined matrix will represent a transformation
+     * that is equivalent to scaling an object by four times and then rotating
+     * it by pi radians.
+     *
+     * In mathematical terms this operation is equivalent to matrix
+     * multiplication. */
+    public static function concat(matrix1:Matrix23, matrix2:Matrix23) {
+        var a = matrix1.a;
+        var b = matrix1.b;
+        var c = matrix1.c;
+        var d = matrix1.d;
+        var tx = matrix1.tx;
+        var ty = matrix1.ty;
+
+        matrix1.a = matrix2.a * a + matrix2.c * b;
+        matrix1.b = matrix2.b * a + matrix2.d * b;
+        matrix1.c = matrix2.a * c + matrix2.c * d;
+        matrix1.d = matrix2.b * c + matrix2.d * d;
+        matrix1.tx = matrix2.a * tx + matrix2.c * ty + matrix2.tx;
+        matrix1.ty = matrix2.b * tx + matrix2.d * ty + matrix2.ty;
     }
 }
